2dcd8bb5f0e488c1091259a4459ebc5561d5f5ef
Peketi <pavani1642002@gmail.com>
2024-11-17 14:13:49 +0530

    first code

---
+++ models/Warehouse.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const warehouseSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    location:{
+        lat:{type:Number,required:true},
+        lng:{type:Number,required:true}
+    }
+});
+module.exports=mongoose.model('Warehouse', warehouseSchema);
---
+++ routes/warehouseRoutes.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const {getAllWarehouses}=require('../controllers/warehouseController');
+const router=express.Router();
+router.get('/warehouses', getAllWarehouses);
+module.exports = router;
